f4472370aa68a049de0f1f8bfef23fb79282f441
Vijay Singh <vjpaleo@gmail.com>
2014-03-12 15:59:27 -0400

    Added modules and changed structure of app.js

--- app.js
+++ app.js
@@ -40,5 +40,23 @@ app.get('/users/:uid', user.detail);
 
 var db = new couchbase.Connection({host: 'localhost:8091', bucket: 'default'});
 
+
+chat_room.sockets.on('connection', function (socket) {
+
+  var parsed = cookie.parse(socket.handshake.headers.cookie);
+
+  joomla.auth_cookies(parsed, function  (j_user) {
+    if (j_user.username === "") {
+      chatter.failure(socket);
+    } else {
+      chatter.connect_chatter({
+        socket: socket,
+        all_sockets: chat_room.sockets,
+        username: j_user.username
+      });
+    }
+  });
+
+});
  
 console.log("Express server listening on port 3000");
